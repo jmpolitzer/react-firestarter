b621bf1d52ad9377e92b9dafb0c16ae78d49a07a
jmpolitzer <jmpolitzer@gmail.com>
2019-02-18 07:12:18 -0600

    adds base methods to firestore provider

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -5,6 +5,26 @@ const FirestoreContext = React.createContext();
 function FirestoreProvider(props) {
   const { fireStore, children } = props;
 
+  const add = (collection, model) = {
+
+  };
+
+  const delete = (collection, id) = {
+
+  }
+
+  const update = (collection, id) = {
+
+  }
+
+  const get = (collection, id) = {
+
+  }
+
+  const getAll = (collection, queries) = {
+    
+  }
+
   return (
     <FirestoreContext.Provider
       value={{
