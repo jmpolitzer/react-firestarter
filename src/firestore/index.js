b537c85ca8cc6128f4ca989e53bf1184736c6418
jmpolitzer <jmpolitzer@gmail.com>
2019-02-19 06:26:50 -0600

    wip - adds basic firestore crud methods

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,34 +1,93 @@
 import React from 'react';
 
+/*
+  TODO:
+  - Add option to enable offline data - https://cloud.google.com/firestore/docs/manage-data/enable-offline
+  - Implement arrayUnion and arrayRemove for updating array values.
+  - Add realtime updates option.
+  - Add support for simple and compound queries.
+  - Add support for ordering and limiting data.
+  - Add support for pagination and query cursors.
+*/
+
 const FirestoreContext = React.createContext();
 
 function FirestoreProvider(props) {
   const { fireStore, children } = props;
+  const db = fireStore;
 
-  const add = async (collection, model) => {
+  const add = async (collection, values) => {
+    try {
+      const docRef = await db.collection(collection).add(values);
 
+      if (docRef) handleFireStoreMessage('success', `Document added with id: ${docRef.id}.`);
+    } catch (error) {
+      handleFireStoreMessage('error', `Error adding document: ${error}.`);
+    }
   };
 
   const remove = async (collection, id) => {
+    try {
+      const docRef = db.collection(collection).doc(id);
+      const deleted = docRef.delete();
 
+      if (deleted) handleFireStoreMessage('success', 'Document successfully deleted.');
+    } catch (error) {
+      handleFireStoreMessage('error', `Error deleting document: ${error}.`);
+    }
   };
 
-  const update = async (collection, id) => {
+  const update = async (collection, id, values) => {
+    try {
+      const docRef = db.collection(collection).doc(id);
+      const updated = docRef.update({
+        ...values,
+        updatedAt: db.FieldValue.serverTimestamp()
+      });
 
+      if (updated) handleFireStoreMessage('success', 'Document successfully updated.');
+    } catch (error) {
+      handleFireStoreMessage('error', `Error updating document: ${error}.`);
+    }
   };
 
   const get = async (collection, id) => {
+    try {
+      const docRef = db.collection(collection).doc(id);
+      const doc = docRef.get();
 
+      if (doc.exists) {
+        return doc.data();
+      } else {
+        handleFireStoreMessage('success', 'No such document.')
+      }
+    } catch (error) {
+      handleFireStoreMessage('error', `Error getting document: ${error}.`);
+    }
   };
 
-  const getAll = async (collection, queries) => {
+  const getAll = async (collection) => {
+    try {
+      const querySnapshot = db.collection(collection).get();
 
+      return querySnapshot.map(doc => doc.data());
+    } catch (error) {
+      handleFireStoreMessage('error', `Error getting documents: ${error}.`);
+    }
+  };
+
+  const handleFireStoreMessage = (type, msg) => {
+    console.log(type, msg);
   };
 
   return (
     <FirestoreContext.Provider
       value={{
-        data: () => console.log("we're connected to firestore!", fireStore)
+        add: add,
+        remove: remove,
+        update: update,
+        get: get,
+        getAll: getAll
       }}
     >
       {children}
